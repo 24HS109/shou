2d42f33fde94854f9bf777a1a80eb8b33234fb1d
24HS109 <24hs109@s.bgu.ac.jp>
2024-12-09 14:01:12 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,29 +3,29 @@
             var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "今日のご飯"; // デフォルトの値
 
-            if (colour === "ピンク" && number === 0) {
+            if (colour === "ピンク" && number === 1) {
                 result = "カレー";
-            } else if (colour === "赤" && number === 1) {
+            } else if (colour === "赤" && number === 2) {
                 result = "ラーメン";
-            } else if (colour === "黒" && number === 2) {
+            } else if (colour === "黒" && number === 3) {
                 result = "牛丼";
-            } else if (colour === "水色" && number === 0) {
+            } else if (colour === "水色" && number === 4) {
                 result = "オムライス";
-            } else if (colour === "白" && number === 1) {
+            } else if (colour === "白" && number === 5) {
                 result = "寿司";
-            } else if (colour === "青" && number === 2) {
+            } else if (colour === "青" && number === 6) {
                 result = "うどん";
-            } else if (colour === "オレンジ" && number === 0) {
+            } else if (colour === "オレンジ" && number === 7) {
                 result = "パスタ";
-            } else if (colour === "緑" && number === 1) {
+            } else if (colour === "緑" && number === 8) {
                 result = "焼肉";
-            } else if (colour === "紫" && number === 2) {
+            } else if (colour === "紫" && number === 9) {
                 result = "天ぷら"; 
-            } else if (colour === "黄緑" && number === 2) {
+            } else if (colour === "黄緑" && number === 10) {
                 result = "そば"; 
-            } else if (colour === "茶色" && number === 2) {
+            } else if (colour === "茶色" && number === 11) {
                 result = "たこ焼き"; 
-            } else if (colour === "銀色" && number === 2) {
+            } else if (colour === "銀色" && number === 12) {
                 result = "お好み焼き";
           
 　　　　　　
