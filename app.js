d3db0d176c069888023c347dc0308c447ead4012
24HS109 <24hs109@s.bgu.ac.jp>
2024-12-09 13:58:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
         function checkFortune() {
             var colour = document.getElementById('colour-select').value;
             var number = parseInt(document.getElementById('number-input').value, 10);
-            var result = "調子"; // デフォルトの値
+            var result = "今日のご飯"; // デフォルトの値
 
             if (colour === "ピンク" && number === 0) {
-                result = "絶好調";
+                result = "カレー";
             } else if (colour === "赤" && number === 1) {
-                result = "好調";
+                result = "ラーメン";
             } else if (colour === "黒" && number === 2) {
-                result = "普通";
+                result = "牛丼";
             } else if (colour === "水色" && number === 0) {
-                result = "絶不調";
+                result = "オムライス";
             } else if (colour === "白" && number === 1) {
-                result = "不調";
+                result = "寿司";
             } else if (colour === "青" && number === 2) {
-                result = "大大絶好調";
+                result = "うどん";
             } else if (colour === "オレンジ" && number === 0) {
-                result = "大絶好調";
+                result = "パスタ";
             } else if (colour === "緑" && number === 1) {
-                result = "大絶不調";
+                result = "焼肉";
             } else if (colour === "紫" && number === 2) {
-                result = "大大絶不調";
-            }
-
-            document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
+                result = "天ぷら"; 
+            } else if (colour === "黄緑" && number === 2) {
+                result = "そば"; 
+            } else if (colour === "茶色" && number === 2) {
+                result = "たこ焼き"; 
+            } else if (colour === "銀色" && number === 2) {
+                result = "お好み焼き";
+          
+　　　　　　
+                document.getElementById('result-output').innerText = "あなたの今日のご飯は【" + result + "】です";
         }
