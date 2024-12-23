63cd05301c7337acc6244e0d029c10afafa7f814
24HS109 <24hs109@s.bgu.ac.jp>
2024-12-23 14:23:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,24 +4,29 @@ function checkFortune() {
     var result = "入力が違います"; // Default value
 
     // Check for valid combinations
-    if (colour === "ピンク" && number === 0) {
-        result = "カレー";
-    } else if (colour === "ピンク" && number === 1) {
-        result = "牛丼";
-    } else if (colour === "ピンク" && number === 2) {
-        result = "オムライス";
-    } else if (colour === "赤" && number === 0) {
-        result = "うどん";
-    } else if (colour === "赤" && number === 1) {
-        result = "ラーメン";
-    } else if (colour === "赤" && number === 2) {
-        result = "たこ焼き";
-    } else if (colour === "青" && number === 0) {
-        result = "寿司";
-    } else if (colour === "青" && number === 1) {
-        result = "パスタ";
-    } else if (colour === "青" && number === 2) {
-        result = "お好み焼き";
+    if (isNaN(number) || number < 0 || number > 2) {
+        result = "無効な数字です。0、1、2のいずれかを入力してください。";
+    } else {
+        // Check for valid combinations
+        if (colour === "ピンク" && number === 0) {
+            result = "カレー";
+        } else if (colour === "ピンク" && number === 1) {
+            result = "牛丼";
+        } else if (colour === "ピンク" && number === 2) {
+            result = "オムライス";
+        } else if (colour === "赤" && number === 0) {
+            result = "うどん";
+        } else if (colour === "赤" && number === 1) {
+            result = "ラーメン";
+        } else if (colour === "赤" && number === 2) {
+            result = "たこ焼き";
+        } else if (colour === "青" && number === 0) {
+            result = "寿司";
+        } else if (colour === "青" && number === 1) {
+            result = "パスタ";
+        } else if (colour === "青" && number === 2) {
+            result = "お好み焼き";
+        }
     }
 
     document.getElementById('result-output').innerText = "あなたの今日のご飯は【" + result + "】です";
